97231a33e31be6ac6b9bbed59e270889503d1e3f
Aleksander Slodczyk <Aleksander.Slodczyk@pb.com>
2024-02-08 10:31:18 +0100

    #2 training 25/01

--- cypress/e2e/justForTest/test_part_1.spec.ts
+++ cypress/e2e/justForTest/test_part_1.spec.ts
@@ -4,15 +4,30 @@
 
 import { baseData } from "../../fixtures/base.json";
 import { controllers } from "../../fixtures/controllers.json";
+import { Helpers } from "../../support/helpers";
 
 describe("Register / login /logout users", () => {
   // Before - odpalenie chroma, login do apki...
   // BeforeEach - clean up przed testem
   // AfterEach ...
   // After
+
+  const helpers = new Helpers();
+  const testUserName = `TestName_${Math.random().toString().slice(2, 12)}`;
+  const testEmail = `${testUserName}@yopmail.com`;
+
   it("Register user test", () => {
     cy.visit(baseData.defaultUrl);
     cy.get("img[src*='logo.png']").should("be.visible");
-    cy.xpath(controllers.static.logo).should("be.visible");
+    helpers.getElement(controllers.static.logo).should("be.visible");
+    helpers.getElement(controllers.buttons.signUp_login).click();
+    helpers.getElement(controllers.static.signUpForm).should("be.visible");
+    helpers
+      .getElement(controllers.static.signUpForm)
+      .contains(controllers.text.newUserSignUp);
+
+    helpers.getElement(controllers.input.signUpName).type(testUserName);
+    helpers.getElement(controllers.input.signUpEmail).type(testEmail);
+    helpers.getElement(controllers.buttons.signUp_button).click();
   });
 });
--- cypress/support/helpers.ts
+++ cypress/support/helpers.ts
@@ -1,3 +1,12 @@
 export class Helpers {
   // metoda do selektora xpath albo css żeby wybierał
+  getElement(selector: string) {
+    if (selector.startsWith("//")) {
+      cy.xpath(selector).should("exist", { timeout: 10000 });
+      return cy.xpath(selector);
+    } else {
+      cy.get(selector).should("exist", { timeout: 10000 });
+      return cy.get(selector);
+    }
+  }
 }
